7ef316c2bb6631e6b651456434a80c80c44d0209
Allstarz600 <67435777+Allstarz600@users.noreply.github.com>
2021-10-09 22:21:11 +0530

    Add files via upload

---
+++ instagram-api.js
@@ -0,0 +1,15 @@
+<!-- BEGIN - FC Instagram - Script -->
+    <script src="instagram/shared/js/FCInstagram.js"></script>
+    <script>
+    jQuery.fn.FCInstagram.accessData = {
+        accessToken: "", // Token
+    };
+    $('#instafeed').FCInstagram({
+        max: 9, // A number between 1 and 25 of photos to show. Default: 9
+        autoplay: true, // Set autoplay video: true/false. Default: false
+        complete: function () { // A callback function to execute after the display of the photos.
+            console.log('completed');
+        }
+    });
+    </script>
+<!-- END - FC Instagram - Script -->
